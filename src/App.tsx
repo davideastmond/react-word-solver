0ddba089c73d0a3454396b2754358b823aea6baf
David Eastmond <davebuck1981@gmail.com>
2023-12-29 22:25:39 -0500

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { useEffect } from "react";
 import "./App.css";
-import { loadWordList } from "./wordlist/word-list";
+import { GameBoard } from "./components/game-board/Game-Board";
 
 function App() {
-  useEffect(() => {
-    renderWordList();
-  }, []);
-
-  const renderWordList = async () => {
-    const list: string[] = await loadWordList();
-    console.log(list);
-  };
-  return (
-    <>
-      <p>App Placeholder</p>
-    </>
-  );
+  return <GameBoard />;
 }
 
 export default App;
