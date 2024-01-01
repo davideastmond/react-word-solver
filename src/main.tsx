36c867865ad1bdfa0d97ed03b1b20a201cec2f16
David Eastmond <davebuck1981@gmail.com>
2024-01-01 07:38:15 -0500

    react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { router } from "./router/router.tsx";
 import theme from "./theme/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
