c8fba4d378d3ce7daa0245edf2c8f87c5aa60d58
David Eastmond <davebuck1981@gmail.com>
2024-01-01 14:28:38 -0500

    fix wording

--- src/pages/help/content/data/About-Content.tsx
+++ src/pages/help/content/data/About-Content.tsx
@@ -5,8 +5,8 @@ export const AboutContent = () => {
   return (
     <StyledContentBox>
       <Typography>
-        React Word Solver is a tool to assist you in solving word puzzles like
-        Wordle, Words with Friends, Scrabble etc.
+        React Word Solver is a tool to assist you in solving word puzzles such
+        as Wordle, Words with Friends, Scrabble etc.
       </Typography>
       <Typography>
         Using the Word Solver's massive word list, you can use the built-in
